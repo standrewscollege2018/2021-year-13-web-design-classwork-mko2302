25f3f0112b5aed39953c4b2caaa98ee63df287d1
mko2302 <mko2302@stacmail.net>
2021-02-09 09:49:28 +1300

    Create home.php

---
+++ tutor/home.php
@@ -0,0 +1,8 @@
+<p>Welcome to St Andrew's College!</p>
+
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Search </h1>
+    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+  </div>
+</div>
