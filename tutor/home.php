61cd944d19c588e23c3aa4e3fe9a9ceafeb1e9b9
mko2302 <mko2302@stacmail.net>
2021-02-09 10:30:50 +1300

    Update home.php

--- tutor/home.php
+++ tutor/home.php
@@ -1,8 +1,11 @@
-<p>Welcome to St Andrew's College!</p>
-
+<!-- home page -->
 <div class="jumbotron jumbotron-fluid">
   <div class="container">
-    <h1 class="display-4">Search </h1>
-    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+    <h1 class="display-3">Welcome to St Andrews College</h1>
+    <h2 class="display-4">Search Students</h2>
+    <form class="form-inline my-2 my-lg-0" action="index.php?page=searchresults" method="post">
+      <input class="form-control mr-sm-2" required type="text" name="search" placeholder="Student name">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
   </div>
 </div>
