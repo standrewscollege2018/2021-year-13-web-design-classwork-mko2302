a7adec580506815e58b6c4a5fceeb6acec7101ad
mko2302 <mko2302@stacmail.net>
2021-03-16 09:08:24 +1300

    Update searchresults.php

--- tutor/searchresults.php
+++ tutor/searchresults.php
@@ -8,7 +8,7 @@
   $search = $_POST['search'];
 
   // forms sql query to find like search
-  $result_sql = "SELECT * FROM student WHERE firstname LIKE '%$search%' OR lastname LIKE '%$search%'";
+  $result_sql = "SELECT * FROM student JOIN tutorgroup ON tutorgroup.tutorgroupID = student.tutorgroupID WHERE firstname LIKE '%$search%' OR lastname LIKE '%$search%'";
 
   // sends sql query to data base
   $result_qry = mysqli_query($dbconnect, $result_sql);
@@ -16,7 +16,7 @@
   // if no results found
   if(mysqli_num_rows($result_qry)==0) {
     // prints no results
-      echo "<h1>No results found</h1>";
+      echo "<p class='dispaly-2 text-centre p-5'>No results found</p>";
     } else {
       // is there are results
       // puts results into assosiative array
@@ -28,12 +28,15 @@
           $firstname = $result_aa['firstname'];
           $lastname = $result_aa['lastname'];
           $photo = $result_aa['photo'];
+          $tutorcode = $result_aa['tutorcode'];
+          $tutorID = $result_aa['tutorgroupID'];
 
-          echo" <div class='col-lg-4'>
+          echo" <div class='col-lg-4 col-md-6'>
                   <div class='card'>
                   <img class='card-img-top' src='images/$photo' alt='Image of $firstname $lastname'>
                     <div class='card-body'>
                       <h5 class='card-title'>$firstname $lastname</h5>
+                      <a href='index.php?page=tutorgroup&tutorgroupID=$tutorgroupID&tutorcode=$tutorcode'>Tutor: $tutorcode</a>
                     </div>
                   </div>
                 </div>";
