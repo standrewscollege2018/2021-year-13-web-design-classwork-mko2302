3a0b2a09521364998af328477d5e6b0a41184e94
mko2302 <mko2302@stacmail.net>
2021-03-16 09:08:37 +1300

    Update home.php

--- tutor/home.php
+++ tutor/home.php
@@ -1,7 +1,7 @@
 <!-- home page -->
 <div class="jumbotron jumbotron-fluid">
   <div class="container">
-    <h1 class="display-3">Welcome to St Andrews College</h1>
+    <h1 class="display-3">Welcome to St Andrew's College</h1>
     <h2 class="display-4">Search Students</h2>
     <form class="form-inline my-2 my-lg-0" action="index.php?page=searchresults" method="post">
       <input class="form-control mr-sm-2" required type="text" name="search" placeholder="Student name">
